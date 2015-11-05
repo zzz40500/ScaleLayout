89ad2803730eae25be877f87e9591f442551e572
轻微 <wyh405003836@126.com>
2015-11-05 16:11:10 +0800

    0.2
    可以简单的配置长宽比,可以设置偏移量.

--- scalelayout/src/main/java/com/mingle/widget/ScaleLayout.java
+++ scalelayout/src/main/java/com/mingle/widget/ScaleLayout.java
@@ -18,44 +18,74 @@ import java.lang.reflect.Type;
 public class ScaleLayout extends RelativeLayout {
 
 
+
+
     public float mScale;
+    private float mOffsetY;
+
     public ScaleLayout(Context context) {
         super(context);
     }
 
+
     public ScaleLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+
 
         init(attrs);
     }
 
+
     private void init(AttributeSet attrs) {
         TypedArray typedArray=getContext().obtainStyledAttributes(attrs, R.styleable.ScaleLayout);
         mScale= typedArray.getFloat(R.styleable.ScaleLayout_scale,-1);
+
+        if(mScale ==-1){
+            float scaleProvideX= typedArray.getFloat(R.styleable.ScaleLayout_scaleProvideX,-1);
+            float scaleProvideY= typedArray.getFloat(R.styleable.ScaleLayout_scaleProvideY,-1);
+            mOffsetY = typedArray.getDimensionPixelOffset(R.styleable.ScaleLayout_offsetY, 0);
+            if(scaleProvideX >0 &&  scaleProvideY>0){
+                mScale=scaleProvideY/scaleProvideX;
+            }
+        }
         typedArray.recycle();
 
+
     }
+
 
     public ScaleLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ScaleLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+
+
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        if(mScale != -1){
-            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec((int) (MeasureSpec.getSize(widthMeasureSpec) * mScale), MeasureSpec.getMode(widthMeasureSpec)));
-
+        if(mScale > 0){
+            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec((int) (MeasureSpec.getSize(widthMeasureSpec) * mScale+mOffsetY), MeasureSpec.getMode(widthMeasureSpec)));
         }else{
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
+    }
 
-   }
 
+    public void setOffsetY(float offsetY) {
+        mOffsetY = offsetY;
+        invalidate();
 
+    }
+
+    public void setWidthAndHeightScale(float scale) {
+        mScale = scale;
+        invalidate();
+    }
 }
